b7622c0640e1ad6ad5a291c0ec406899e337412f
FRIASMIGUEL <jmfrias1950@gmail.com>
2023-04-25 22:28:58 -0300

    alterações no teste automatizado

--- cypress/e2e/cypress/e2e/login.cy.js
+++ cypress/e2e/cypress/e2e/login.cy.js
@@ -2,9 +2,17 @@
 
 context('Funcionalidade Login', () =>{
 
-           
+    beforeEach(() => {cy.visit('http://lojaebac.ebaconline.art.br/minha-conta')
+        
+    });
+    
+    
+    afterEach(() => { cy.screenshot()  
+        
+    });
+     
     it('Deve fazer login com sucesso', () =>{
-    cy.visit('http://lojaebac.ebaconline.art.br/minha-conta')
+    
     cy.get('.woocommerce-form > :nth-child(1) > label').type('aluno_ebac@teste.com')
     cy.get('#password'). type('teste@teste.com')
     cy.get('.woocommerce-form > .button').click()
@@ -16,7 +24,7 @@ context('Funcionalidade Login', () =>{
     
     
     it('Deve exibir uma mensagem de erro ao inserir usuário inválido', () =>{
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta')
+       
         cy.get('.woocommerce-form > :nth-child(1) > label').type('@teste.com')
         cy.get('#password'). type('teste@teste.com')
         cy.get('.woocommerce-form > .button').click()
@@ -25,7 +33,7 @@ context('Funcionalidade Login', () =>{
     })
     
     it('Deve exibir uma mensagem de erro ao inserir senha inválida', () =>{
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta')
+        
         cy.get('.woocommerce-form > :nth-child(1) > label').type('aluno_ebac@teste.com')
         cy.get('#password'). type('teste@teste')
         cy.get('.woocommerce-form > .button').click()
---
+++ cypress/e2e/cypress/e2e/pre cadastro.cy.js
@@ -0,0 +1,33 @@
+/// <reference types="cypress"/>
+
+var faker = require('faker');
+
+describe('Funcionalidade Pre Cadastro', () =>{
+
+    beforeEach(() => {cy.visit('http://lojaebac.ebaconline.art.br/minha-conta')
+        
+    });
+
+
+    afterEach(() => { cy.screenshot()  
+        
+    });
+     
+    it('Deve completar o pre cadastro com sucesso', () =>{
+let nomeFaker = faker.name.firstName()
+let sobrenomeFaker = faker.name.lastName()
+let emailFaker = faker.internet.email(nomeFaker)
+
+        cy.get('.register > :nth-child(1) > label').type(emailFaker)       
+        cy.get('#reg_password').type('ebac@teste$3456')
+        cy.get(':nth-child(4) > .button').click()
+        cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a').click()
+
+        cy.get('#account_first_name').type(nomeFaker)
+        cy.get('#account_last_name').type(sobrenomeFaker)
+        cy.get('.woocommerce-Button').click()
+        cy.get('.woocommerce-message').should('contain','Detalhes da conta modificados com sucesso')
+
+        });
+
+});
